ea3b3aeb1d7fac5754bb5147c9f5682cbec3834b
muhammad fakhri <12muhammadfakhri@gmail.com>
2022-08-02 21:40:08 +0700

    fixing:cannot deploy

--- src/components/user/SignUpForm.jsx
+++ src/components/user/SignUpForm.jsx
@@ -16,7 +16,6 @@ const SignUpForm = ({
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
               className="w-full"
-              alt="Phone image"
             />
           </div>
           <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
